8536bed06447ac4230f5ca5a85b4400b587a9912
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-26 21:56:46 -0700

    created a constructor and a boolean function to check the presence ofthe rear in the given array

--- src/QueuA.java
+++ src/QueuA.java
@@ -1,5 +1,17 @@
 public class QueuA {
-    public st
+    public static class Queu{
+        static int [] arr;
+        static int rear= -1;
+        static int size;
+        Queu(int n){
+            arr= new int[n];
+            this.size =n;
+        }
+        public static boolean isEmpty(){
+            return rear==-1;
+        }
+
+    }
     public static void main(String[] args) {
 
     }
